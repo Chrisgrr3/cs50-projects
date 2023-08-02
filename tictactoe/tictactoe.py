bb17f6dc2b3a00807aab635f41e565b9fae41523
Christopher Guerrero <chrisgrr3@gmail.com>
2023-08-01 19:34:59 -0700

    Implemented function to determine the winner of tic-tac-toe game

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -22,7 +22,7 @@ def player(board):
     """
     count = 0
     for row in board:
-        for column in row:
+        for column in board[row]:
             if board[row][column] != EMPTY:
                 count += 1
 
@@ -38,7 +38,7 @@ def actions(board):
     """
     possible_moves = set()
     for row in board:
-        for column in row:
+        for column in board[row]:
             if board[row][column] == EMPTY:
                 possible_moves.add((row, column))
     return possible_moves
@@ -48,20 +48,41 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    import copy
+
     current_player = player(board)
+    board_copy = copy.deepcopy(board)
     if action not in actions(board):
         raise Exception("That is not a valid move")
     else:
         row = action[0]
         column = action[1]
-        board[row][column] = current_player
+        board_copy[row][column] = current_player
+        return board_copy
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    # Check for diagonal winner
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+        return board[0][2]
+
+    for row in board:
+        # Check winner horizontally
+        if board[row][0] == board[row][1] and board[row][1] == board[row][2]:
+            return board[row][0]
+        # Check winner vertically
+        if row == 0:
+            for column in board[row]:
+                if (
+                    board[row][column] == board[row + 1][column]
+                    and board[row + 1][column] == board[row + 2][column]
+                ):
+                    return board[row][column]
 
 
 def terminal(board):
